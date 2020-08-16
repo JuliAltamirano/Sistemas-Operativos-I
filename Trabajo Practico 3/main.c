ff4fc144dd949e2682399a0b0674461be577ed34
JuliAltamirano <altamiranojuli98@gmail.com>
2020-08-16 01:18:26 -0300

    the other methods

--- Trabajo Practico 3/main.c
+++ Trabajo Practico 3/main.c
@@ -8,26 +8,31 @@ int main(){
     int result;
     char option[10];
     size_t size_option;
-    
-    errors.is_error = false;    // Initialize 'is_error' varible
+
+    // Initialize 'Errors' structure
+    errors.is_error = false;
+    errors.error_number = 0;
+    for ( int i = 0; i < 500; i++ ) 
+		errors.device[i] = 0;
    
     do {
         opt_fail = false;
-        printf( "Ingrese la opcion que desea realizar:\n"
-                "a) Encriptar cadena.\n"
-                "b) Desencriptar cadena.\n");
+        printf("Ingrese la opcion que desea realizar:\n"
+               "a) Encriptar cadena.\n"
+               "b) Desencriptar cadena.\n");
 
         result = scanf("%[^\n]%*c", option);
         size_option = strlen(option);
 
-        if(result == EOF){
-            printf("ERROR\n");              // TODO: change message
+        if( result == EOF ){
+            errors.is_error = true;
+            errors.error_number = 1;
+            errorMessage();
             return EOF;
         }
 
         if( size_option != 1 || ( option[0] != 'a' && option[0] != 'b' )){
-            
-            printf("OPCION INCORRECTA\n");  // TODO: change message
+            printf("OPCION INCORRECTA\n");
             opt_fail = true;
         }
         
@@ -48,6 +53,22 @@ int main(){
 }
 
 void errorMessage() {
-    // TODO
-    return;
+    
+    switch( errors.error_number )
+    {
+        case 1:
+            printf("ERROR: no se pudo obtener los caracteres ingresados por el usuario\n");
+            break;
+        case 2:
+            printf("ERROR: no se pudo abrir el archivo del dispositivo '%s'\n", errors.device);
+            break;
+        case 3:
+            printf("ERROR: no se pudo escribir en el archivo del dispositivo '%s'\n", errors.device);
+            break;
+        case 4:
+            printf("ERROR: no se pudo leer el archivo del dispositivo '%s'\n", errors.device);
+            break;
+        default:
+            break;
+    }
 }
